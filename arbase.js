677aaeca8c4d7c8bdb70ddf9c15bd20609774be8
sofias <sofias@radikalismus.com>
2017-12-09 19:27:21 +0100

    what i had so far
    not compatible to anything

--- arbase.js
+++ arbase.js
@@ -1,98 +1,31 @@
-(function (arbase) {
-    function mod (a, b) {
-        return ((a % b) + b) % b;
-    }
+'use strict'
 
-    function div (a, b) {
-        return Math.floor(a / b);
-    }
+const mod = (num, by) => (num % by + Math.abs(by)) % by
 
-    function divmod (a, b) {
-        var obj = [div(a, b), mod(a, b)];
-        obj.div = obj[0];
-        obj.mod = obj[1];
-        return obj;
-    }
+const divmod = (num, by) => ({
+	div: Math [
+		by > 0 ? 'floor' : 'ceil'
+	] (num / by),
+	mod: mod (num, by)
+})
 
-    function log (base, num) {
-        return Math.log(num) / Math.log(base);
-    }
+const mulmod = (num, by) => ({
+	mul: (num * by) % 1,
+	mod: Math.floor ((num * by))
+})
 
-    arbase.translationTable = {}; // default for translationTable option
-    arbase.seperator = '.';       // default for seperator option
-    arbase.ignore = [','];        // default for ignore option
-    arbase.defaultBase = 10;      // default for base argument
-    arbase.rangeCheck = false;    // default for rangeCheck option
-
-    /* [ Generate translation table */
-    var i = 0, a = 'a'.charCodeAt(0), z = 'z'.charCodeAt(0), delta = 'A'.charCodeAt(0) - a;
-
-    for (i = 0; i < 10; i++) arbase.translationTable[i] = i;
-
-    for (i = a; i <= z; i++) {
-        arbase.translationTable[String.fromCharCode(i)] = i - a + 10;
-        arbase.translationTable[i - a + 10] = String.fromCharCode(i);
-        arbase.translationTable[String.fromCharCode(i + delta)] = i - a + 10;
-        arbase.translationTable[i - a + 10] = String.fromCharCode(i + delta);
-    }
-    /* Generate translation table } */
-
-    arbase.decode = function (numIn, base, opt) {
-        if (!opt) opt = {};
-        if (!base) base = arbase.defaultBase;
-        var numOut = 0,
-            length = numIn.length,
-            rangeCheck      = opt['rangeCheck']       || arbase.rangeCheck,
-            seperatorSymbol = opt['seperator']        || arbase.seperator,
-            ignore          = opt['ignore']           || arbase.ignore,
-            table           = opt['translationTable'] || arbase.translationTable;
-
-        numIn = numIn.toString();
-
-        var seperatorPos = numIn.indexOf(seperatorSymbol);
-
-        if (seperatorPos < 0) seperatorPos = length;
-        seperatorPos -= 1;
-        numIn = numIn.replace(new RegExp('\\' + seperatorSymbol, 'g'), '');
-        length = numIn.length;
-
-        for (i = 0; i < length; i++) {
-            if (rangeCheck && (table[numIn[i]] < 0 || (Math.abs(table[numIn[i]]) > Math.abs(base) - 1)))
-                throw new Error('Arbase Error: ' + numIn[i] + ' out of range');
-            numOut += table[numIn[i]] * (Math.pow(base, seperatorPos - i))
-        }
-
-        return numOut;
-    };
-
-    arbase.encode = function (num, base, opt) {
-        if (!base) base = arbase.defaultBase;
-        if (!opt) opt = {};
-
-        var numOut = '',
-            seperatorSet = false,
-            seperatorSymbol = opt['seperator']        || arbase.seperator,
-            ignore          = opt['ignore']           || arbase.ignore,
-            table           = opt['translationTable'] || arbase.translationTable,
-            precision       = opt['precision']        || 0.00001;
-
-        num = Number(num);
-
-        var periodPrinted = false;
-        var numberOfDigits = Math.ceil(log(base, num));
-
-        while (num > precision || numberOfDigits > 0) {
-            if (numberOfDigits <= 0 && !periodPrinted) {
-                periodPrinted = true;
-                numOut += seperatorSymbol;
-            }
-            var digitValue = Math.pow(base, numberOfDigits - 1);
-            var div = divmod(num, digitValue);
-            numOut += table[div.div];
-            num = div.mod;
-            --numberOfDigits;
-        }
-
-        return numOut;
-    };
-}(typeof module !== 'undefined' ? module.exports : (arbase = {})));
+const toBase = (div, base) => {
+	let point = 0
+	const result = []
+	let afterpoint = div
+	while (afterpoint % 1) {
+		afterpoint *= base
+		point ++
+	}
+	div *= base ** point
+	while (div != 0) {
+		({div, mod} = divmod (div, base))
+		result.push (mod)
+	}
+	return result.map (x => x.toString (36)).reverse ().join('')
+}
